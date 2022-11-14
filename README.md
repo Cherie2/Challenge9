b581e495e5f35602a11b64b008381252a3f59065
Cherie2 <cwalk280@yahoo.com>
2022-11-14 00:26:14 -0800

    Commit with revised starter code to generate readme

---
+++ README.md
@@ -0,0 +1,36 @@
+
+# Test
+  
+## Table Of Contents
+  - [Project Description](#Description)
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [License](#License)
+  - [Contribution](#Contribution)
+  - [Tests](#Tests)
+  - [Questions](#Questions)
+  
+## Description
+  This is a test
+
+## Installation
+  Just need inquirer installed
+
+## Usage
+  answer prompts and readme is generated
+
+## License
+  MIT
+
+## Contribution
+  Recommendations welcome
+
+## Tests
+  n/a
+
+## Questions
+ *Any questions please contact me*
+  
+  cwalk280@yahoo.com
+
+  [Cherie2](https://github.com/Cherie2)
---
+++ index.js
@@ -0,0 +1,74 @@
+//Included packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./generateMarkdown");
+
+//Create an array of questions for user input in ReadMe file
+const questions = [
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the Project Title?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'What is the Project Description?',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install application?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What are the instructions to use application?',
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'How to contribute to the application?',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'How to run tests?',
+      },
+      {
+        type: 'input',
+        name: 'GitHub',
+        message: 'What is your GitHub Username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What is the license?',
+        choices:['MIT','ISC'],
+      },
+];
+
+//Function initialize app and creates readme
+function init() {
+    return inquirer.prompt(questions)
+    .then((data) => {
+        const genMark = generateMarkdown(data)
+        fs.writeFile("Readme.md", genMark, function(err){
+            if(err){
+                console.log("Unable to save file", err)
+            }else{
+                console.log("Successful generation of Readme.md file")
+            }
+        })
+    })
+    .catch((err) => console.error(err))
+}
+
+//Function call to initialize app
+init();
+
